8e9680d0b9e5a45bc1b6d839975c9a5129caacd3
Fernando González <rmfernando@outlook.com>
2020-07-23 23:33:24 -0600

    Delete and Get implemented

--- my-favorite-cars-SPA/src/app/cars/cars.component.ts
+++ my-favorite-cars-SPA/src/app/cars/cars.component.ts
@@ -9,17 +9,63 @@ import { HttpClient } from '@angular/common/http';
 export class CarsComponent implements OnInit {
   cars: any;
   today: number = Date.now();
+  xi: any;
+  x2: any;
+
+  test: {
+    id: 1;
+    name: 'Lamborghini Huracan';
+    url: 'https://cdn.carbuzz.com/gallery-images/2020-lamborghini-huracan-evo-front-angle-view-carbuzz-529237-1600.jpg';
+    motor: 'V10';
+  };
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getValues();
+    this.getCars();
+    //this.postCar();
+    //this.deleteCar(1023);
   }
 
-  getValues() {
+  postCar() {
+    interface CarsInter {
+      id: number;
+      name: string;
+    }
+    this.http
+      .post<CarsInter>('http://localhost:5000/api/cars/', {
+        name: 'Lamborghini Huracan Spyder',
+        brand: 'Lamborghini',
+        model: '2020',
+        url:
+          'https://cdn.carbuzz.com/gallery-images/2020-lamborghini-huracan-evo-spyder-carbuzz-551818.jpg',
+        motor: 'V10',
+      })
+      .subscribe(
+        (response) => {
+          this.xi = response.id;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  getCars() {
     this.http.get('http://localhost:5000/api/cars/').subscribe(
       (response) => {
         this.cars = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteCar(id) {
+    this.http.delete<any>('http://localhost:5000/api/cars/' + id).subscribe(
+      (res) => {
+        this.x2 = res.id;
       },
       (error) => {
         console.log(error);
